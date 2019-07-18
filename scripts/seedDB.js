cf9568f003f85fff17b7b26ba47b9b28abdb13a3
JoJo <shelton.jocelyn@gmail.com>
2019-07-18 19:48:58 -0400

    mongo code

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -9,11 +9,11 @@ mongoose.connect(
 );
 
 const bookSeed = {
-    authors: [""],
-    description: "",
-    image: "",
-    link: "",
-    title: "",
+    authors: ["Francis Scott Fitzgerald"],
+    description: "The Roaring 20's!",
+    image: "https://books.google.com/books/content?id=HestSXO362YC&printsec=frontcover&img=1&zoom=1&edge=curl&imgtk=AFLRE70Q2VjgYVN75fnbJ3KZWV85nlIVodgvTAHFdux6M2nab6sJgicXPsPBW8SpjXcsTfoPGGjBue-7UlovXDLtVGPKS62SihUWLQEb2pKvSaBd7OEtIjr4nSK4dh0-fuLkfvfYw3Z5",
+    link: "https://books.google.com/books?id=HestSXO362YC&dq=The+great+gatsby&hl=&source=gbs_api",
+    title: "The Great Gatsby",
 }
 
 db.Book
